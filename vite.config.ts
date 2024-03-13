3ed889eea2d0eea8787a9a80504afbe73d8a2666
Leonardo Villagrán <leonardovillagranchicago@gmail.com>
2024-03-13 14:01:52 -0300

    vite config added

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/portfolio-ts/',
   plugins: [react()],
+  build: {
+    assetsDir: 'assets', // Carpeta de salida para los assets
+    chunkSizeWarningLimit: 2000, // Set the warning limit to 1000 kB
+    outDir: 'dist', // Carpeta de salida para los archivos construidos
+    sourcemap: false, // Desactivar mapas de origen para producción
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // default: 3000
+  }, 
 })
